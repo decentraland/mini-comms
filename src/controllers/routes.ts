fbe7b255533709af262d9bfffe3011be4b2365fd
Mariano Goldman <mariano.goldman@decentraland.org>
2022-09-13 18:22:26 -0300

    fix build

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -2,11 +2,13 @@ import { Router } from '@well-known-components/http-server'
 import { GlobalContext } from '../types'
 import { statusHandler } from './handlers/status-handler'
 import { websocketHandler } from './handlers/room-handler'
+import { pingHandler } from './handlers/ping-handler'
 
 // We return the entire router because it will be easier to test than a whole server
 export async function setupRouter(_: GlobalContext): Promise<Router<GlobalContext>> {
   const router = new Router<GlobalContext>()
 
+  router.get('/ping', pingHandler)
   router.get('/rooms/status', statusHandler)
   router.get('/rooms/:roomId', websocketHandler)
 
