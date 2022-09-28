7bfefdd30d6202bbc696851b19c3d95f884775cf
Hugo Arregui <hugo@decentraland.org>
2022-09-28 18:21:57 -0300

    fix metrics

--- src/metrics.ts
+++ src/metrics.ts
@@ -10,14 +10,10 @@ export const metricDeclarations = {
     type: IMetricsComponent.CounterType,
     labelNames: ['pathname']
   },
-  explorer_bff_build_info: {
+  mini_comms_build_info: {
     help: 'Explorer BFF build info.',
     type: IMetricsComponent.GaugeType,
-    labelNames: ['commitHash', 'ethNetwork', 'commsProtocol']
-  },
-  explorer_bff_connected_users: {
-    help: 'Explorer BFF current connected users.',
-    type: IMetricsComponent.GaugeType
+    labelNames: ['commitHash', 'ethNetwork']
   }
 }
 
