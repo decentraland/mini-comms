5d47d047729a77d5486dbeae4ccf3005f13291da
menduz <github@menduz.com>
2023-02-16 15:32:17 +0000

    update and reduce dependencies

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
-PROTOBUF_VERSION = 3.19.1
 UNAME := $(shell uname)
 
 export PATH := node_modules/.bin:/usr/local/include/:protoc3/bin:$(PATH)
@@ -7,32 +6,6 @@ ifneq ($(CI), true)
 LOCAL_ARG = --local --verbose --diagnostics
 endif
 
-ifeq ($(UNAME),Darwin)
-PROTOBUF_ZIP = protoc-$(PROTOBUF_VERSION)-osx-x86_64.zip
-else
-PROTOBUF_ZIP = protoc-$(PROTOBUF_VERSION)-linux-x86_64.zip
-endif
-
-protoc3/bin/protoc:
-	@# remove local folder
-	rm -rf protoc3 || true
-
-	@# Make sure you grab the latest version
-	curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v$(PROTOBUF_VERSION)/$(PROTOBUF_ZIP)
-
-	@# Unzip
-	unzip $(PROTOBUF_ZIP) -d protoc3
-	@# delete the files
-	rm $(PROTOBUF_ZIP)
-
-	@# move protoc to /usr/local/bin/
-	chmod +x protoc3/bin/protoc
-
-install: protoc3/bin/protoc
-	npm ci
-	npm i -S google-protobuf@$(PROTOBUF_VERSION)
-	npm i -S @types/google-protobuf@latest
-
 test: build
 	touch .env
 	node_modules/.bin/jest --forceExit --detectOpenHandles --coverage --verbose $(TESTARGS)
@@ -40,15 +13,7 @@ test: build
 test-watch:
 	node_modules/.bin/jest --detectOpenHandles --colors --runInBand --watch $(TESTARGS) --coverage
 
-src/proto/ws-comms-rfc-5.ts: protoc3/bin/protoc node_modules/@dcl/protocol/kernel/comms/ws-comms-rfc-5.proto
-	protoc3/bin/protoc \
-		--plugin=./node_modules/.bin/protoc-gen-ts_proto \
-		--ts_proto_opt=esModuleInterop=true,returnObservable=false,outputServices=generic-definitions \
-		--ts_proto_out="$(PWD)/src/proto" \
-		-I="$(PWD)/node_modules/@dcl/protocol/kernel/comms" \
-		"$(PWD)/node_modules/@dcl/protocol/kernel/comms/ws-comms-rfc-5.proto"
-
-build: src/proto/ws-comms-rfc-5.ts
+build:
 	@rm -rf dist || true
 	@mkdir -p dist
 	@./node_modules/.bin/tsc -p tsconfig.json
@@ -63,6 +28,9 @@ profile: build
 		--unhandled-rejections=strict \
 		--inspect \
 		dist/index.js
+
+remove-deps: # removes server dependencies before publishing
+	echo 1
 
 lint:
 	@node_modules/.bin/eslint . --ext .ts
--- src/logic/handle-linear-protocol.ts
+++ src/logic/handle-linear-protocol.ts
@@ -28,47 +28,58 @@ export async function handleSocketLinearProtocol(
   try {
     // process the messages
     /// 1. the remote client sends their authentication message
-    const { peerIdentification } = await channel.yield(1000, 'Timed out waiting for peer identification')
+    const msg = await channel.yield(1000, 'Timed out waiting for peer identification')
 
-    if (!peerIdentification) throw new Error('Invalid protocol. peerIdentification packet missed')
-    if (!EthAddress.validate(peerIdentification.address))
+    if (msg.message?.$case !== 'peerIdentification')
+      throw new Error('Invalid protocol. peerIdentification packet missed')
+    if (!EthAddress.validate(msg.message.peerIdentification.address))
       throw new Error('Invalid protocol. peerIdentification has an invalid address')
 
     const challengeToSign = 'dcl-' + Math.random().toString(36)
-    const alreadyConnected = components.rooms.isAddressConnected(normalizeAddress(peerIdentification.address))
+    const alreadyConnected = components.rooms.isAddressConnected(
+      normalizeAddress(msg.message.peerIdentification.address)
+    )
     logger.debug('Generating challenge', {
       challengeToSign,
-      address: peerIdentification.address,
+      address: msg.message.peerIdentification.address,
       alreadyConnected: alreadyConnected + ''
     })
 
     if (socket.readyState !== socket.OPEN) throw new Error('Connection lost')
     /// 2. send the challenge back to the client
-    socket.send(craftMessage({ challengeMessage: { alreadyConnected, challengeToSign } }), (err) => {
-      if (err) {
-        logger.error(err)
-        socket.terminate()
-        channel.close()
+    socket.send(
+      craftMessage({
+        message: {
+          $case: 'challengeMessage',
+          challengeMessage: { alreadyConnected, challengeToSign }
+        }
+      }),
+      (err) => {
+        if (err) {
+          logger.error(err)
+          socket.terminate()
+          channel.close()
+        }
       }
-    })
+    )
 
     /// 3. wait for the confirmation message
-    const { signedChallengeForServer } = await channel.yield(1000, 'Timed out waiting for signed challenge response')
+    const msg2 = await channel.yield(1000, 'Timed out waiting for signed challenge response')
 
-    if (!signedChallengeForServer) {
+    if (msg2.message?.$case !== 'signedChallengeForServer') {
       throw new Error('Invalid protocol. signedChallengeForServer packet missed')
     }
 
     const result = await Authenticator.validateSignature(
       challengeToSign,
-      JSON.parse(signedChallengeForServer.authChainJson),
+      JSON.parse(msg2.message.signedChallengeForServer.authChainJson),
       components.ethereumProvider
     )
 
     if (result.ok) {
-      logger.debug(`Authentication successful`, { address: peerIdentification.address })
+      logger.debug(`Authentication successful`, { address: msg.message.peerIdentification.address })
 
-      components.rooms.addSocketToRoom(socket, normalizeAddress(peerIdentification.address), room)
+      components.rooms.addSocketToRoom(socket, normalizeAddress(msg.message.peerIdentification.address), room)
     } else {
       logger.error(`Authentication failed`, { message: result.message } as any)
       throw new Error('Authentication failed')
